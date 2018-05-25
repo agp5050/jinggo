a6b1c674883a547da21975223d7f9549d0c8dd93
wangyj <wangyj@soonfor.com>
2018-05-25 15:38:48 +0800

    spring 4.x demo

---
+++ jinggo-springbook-4xdo/4xd0-ch02/src/main/java/ml/jinggo/ex/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by gz12 on 2018-05-25.
+ */
+package ml.jinggo.ex;
---
+++ jinggo-springbook-4xdo/4xd0-ch02/src/main/java/ml/jinggo/ex/service/UserService.java
@@ -0,0 +1,49 @@
+package ml.jinggo.ex.service;
+
+import ml.jinggo.ex.dao.LoginLogDao;
+import ml.jinggo.ex.dao.UserDao;
+import ml.jinggo.ex.domain.LoginLog;
+import ml.jinggo.ex.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+	private UserDao userDao;
+	private LoginLogDao loginLogDao;
+
+
+	public boolean hasMatchUser(String userName, String password) {
+		int matchCount =userDao.getMatchCount(userName, password);
+		return matchCount > 0;
+	}
+	
+	public User findUserByUserName(String userName) {
+		return userDao.findUserByUserName(userName);
+	}
+
+	@Transactional
+    public void loginSuccess(User user) {
+		user.setCredits( 5 + user.getCredits());
+		LoginLog loginLog = new LoginLog();
+		loginLog.setUserId(user.getUserId());
+		loginLog.setIp(user.getLastIp());
+		loginLog.setLoginDate(user.getLastVisit());
+        userDao.updateLoginInfo(user);
+        loginLogDao.insertLoginLog(loginLog);
+	}
+
+	@Autowired
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Autowired
+	public void setLoginLogDao(LoginLogDao loginLogDao) {
+		this.loginLogDao = loginLogDao;
+	}
+}
